bfaab3d983f0f20a822ecd198a7168ab312a3465
dvhi <131928785+dvhi@users.noreply.github.com>
2023-09-27 17:53:46 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+var quick_draw_data_set = ["airplane","alarm clock","ambulance","angel","animal migration","ant","anvil","apple","arm","asparagus","axe","backpack","banana","bandage","barn","baseball","baseball"]
+
+random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
+console.log(quick_draw_data_set[random_number]);
+sketch = quick_draw_data_set[random_number];
+document.getElementById('sketch_name').innerHTML = 'Sketch To be Drawn: ' + sketch;
+
+timer_counter = 0;
+timer_check = "";
+drawn_sketch = "";
+answer_holder = "";
+score = 0;
+
+function updateCanvas() {
+  background("white");
+  random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
+  console.log(quick_draw_data_set[random_number]);
+  sketch = quick_draw_data_set[random_number];
+  document.getElementById('sketch_name').innerHTML = 'Sketch To be Drawn: ' + sketch;
+}
+
+function preload() {
+  classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function setup() {
+  canvas.mouseReleased(classifyCanvas);
+  canvas = createCanvas(280, 280);
+  canvas.center();
+  background("white");
+}
+
+
+function draw()
+
+  check_sketch();
+  if(drawn_sketch == sketch)
+  {
+    answer_holder = "set"
+    score++;
+    document.getElementById('score').innerHTML = 'Score: ' + score;
+  }
+
+  strokeWeight(13);
+  stroke(0);
+  if(mouseIsPressed) {
+  line(pmouseX, pmouseY, mouseX, mouseY);
+
+
+}
+
+function check_sketch()
+{
+  timer_counter++;
+  document.getElementById('time').innerHTML = 'Timer: ' + timer_counter;
+  console.log(timer_counter)
+  if(timer_counter > 400)
+    {
+      timer_counter = 0;
+      timer_check = "completed"
+    }
+    if(timer_check =="completed" || answer_holder == "set")
+    {
+      timer_check = "";
+      answer_holder = "";
+      updateCanvas();
+    }
+
+}
+
+function classifyCanvas() {
+  classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+  if (error) {
+      console.error(error);
+  }
+  console.log(results);
+  document.getElementById('label').innerHTML = 'Label: ' + results[0].label;
+  document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence* 100) + '%';
+
+  utterThis = new SpeechSynthesisUtterance(results[0].label);
+  synth.speak(utterThis);
+}
+
